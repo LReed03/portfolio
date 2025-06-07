f144802e3f2257605a7bde56e6e5525dce0c0c0d
Lreed03 <lreed0383@gmail.com>
2025-06-06 22:34:45 -0600

    Minor changes

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,10 @@ function Header() {
   // Next step here is to add a effect when I scroll into view of each part
   return (
     <div className="container">
-      <ScrollLink className="item" to="home" spy activeClass="link-active">Home</ScrollLink>
-      <ScrollLink className="item" to="about" spy activeClass="link-active" offset={-250}>About</ScrollLink>
-      <ScrollLink className="item" to="projects" spy activeClass="link-active" offset={-250}>Projects</ScrollLink>
-      <ScrollLink className="item" to="contact" spy activeClass="link-active" offset={-250}>Contact</ScrollLink>
+      <ScrollLink className="item" to="home" spy activeClass="link-active" offset={-100}>Home</ScrollLink>
+      <ScrollLink className="item" to="about" spy activeClass="link-active" offset={-400}>About</ScrollLink>
+      <ScrollLink className="item" to="projects" spy activeClass="link-active" offset={-400}>Projects</ScrollLink>
+      <ScrollLink className="item" to="contact" spy activeClass="link-active" offset={-400}>Contact</ScrollLink>
     </div>
   );
 }
