b6fc878deae0f26381d5e43a794f45cd6768cb0f
Lreed03 <lreed0383@gmail.com>
2025-06-09 16:34:05 -0600

    Created outline of about section

--- src/About.js
+++ src/About.js
@@ -4,7 +4,17 @@ import './About.css';
 function About(){
     return(
         <div className="about-container">
-            <p>About</p>
+            <div className="about">
+                <img src="" alt="Headshot" className="pic"></img>
+                <div className="text">
+                    <h2>About Me</h2>
+                    <h5>Software Engineering Student</h5>
+                    <p>Filler content for now fill this in later yuyuyuyuyuyuy</p>
+                    <div className="link">
+                        <a href="#" className="resume">Resume</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
