412988c40006bc04137e0629a84d7d946201d4ba
Lreed03 <lreed0383@gmail.com>
2025-06-10 18:11:35 -0600

    Outline of contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,16 @@ import './Contact.css';
 function Contact(){
     return(
         <div className="contact-container">
-            <p>Contact</p>
+            <form action="" className="contact">
+                <div className="contact-title">
+                    <h2>Get in touch</h2>
+                    <hr/>
+                </div>
+                <input type="text" name="name" placeholder="Your Name" className="contact-inputs" required></input>
+                <input type="email" name="email" placeholder="Your Email" className="contact-inputs" required></input>
+                <textarea name="message" placeholder="Your Message" className="contact-inputs" required></textarea>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
