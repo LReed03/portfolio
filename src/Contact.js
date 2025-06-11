71d146e1481b075d0d6b92de01ba36f26d4d8822
Lreed03 <lreed0383@gmail.com>
2025-06-10 18:22:36 -0600

    Finished the web contact form

--- src/Contact.js
+++ src/Contact.js
@@ -4,11 +4,12 @@ import './Contact.css';
 function Contact(){
     return(
         <div className="contact-container">
-            <form action="" className="contact">
+            <form action="https://api.web3forms.com/submit" method="POST" className="contact">
                 <div className="contact-title">
                     <h2>Get in touch</h2>
                     <hr/>
                 </div>
+                <input type="hidden" name="access_key" value="02368db9-7726-44bb-85ca-db9349f9ee1e"></input>
                 <input type="text" name="name" placeholder="Your Name" className="contact-inputs" required></input>
                 <input type="email" name="email" placeholder="Your Email" className="contact-inputs" required></input>
                 <textarea name="message" placeholder="Your Message" className="contact-inputs" required></textarea>
