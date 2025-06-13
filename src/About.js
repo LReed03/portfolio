8ef4c08aa5e940c8b3f1349a048a38a27cd44029
Lreed03 <lreed0383@gmail.com>
2025-06-12 21:10:01 -0600

    Changed the about me section

--- src/About.js
+++ src/About.js
@@ -13,7 +13,12 @@ function About(){
                     <div className="text">
                         <h2>About Me</h2>
                         <h5>Software Engineering Student</h5>
-                        <p>Filler content for now fill this in later yuyuyuyuyuyuy</p>
+                        <p>Hey! I’m a Software Engineering student who’s into building cool and useful things with code.
+                            Right now, I’m learning a bit of everything — from front-end stuff like HTML/CSS and JavaScript to back-end tools like Java and databases. 
+                            I’m especially into creating apps that are actually fun or helpful to use.
+                            I’m the kind of person who loves figuring things out, whether it’s fixing a bug that won’t go away or 
+                            learning a new tech stack just to see what it can do.
+                            Let’s build something awesome!</p>
                         <div className="link">
                             <a href="" className="resume">Resume</a>
                         </div>
