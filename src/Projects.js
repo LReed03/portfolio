51b7c17dc0c9252a9aeeb6c71abd940b98f59f65
Lreed03 <lreed0383@gmail.com>
2025-06-23 11:08:54 -0600

    Implement code changes to enhance functionality and improve performance

--- src/Projects.js
+++ src/Projects.js
@@ -7,11 +7,11 @@ function Projects(){
         <div className="projects-container">
             <h2>Projects</h2>
             <ProjectItem img={"powerlifting.png"} 
-            description = {"A full-stack application designed for strength athletes and coaches to log, analyze, and optimize powerlifting performance using data-driven insights."} 
+            description = {"A full-stack application built to help strength athletes and coaches analyze competition preformance. This app fetches athlete data from openpowerlifting.org using Beautiful Soup in Python via Flask. The frontend UI is designed through React."} 
             url={"https://github.com/LReed03/powerlifting-analysis"}
             title={"Powerlifting Web App"}/>
-            <ProjectItem img={""}
-            description = {"CRUD STUFF"}
+            <ProjectItem img={"ENSF380UML.png"}
+            description = {"Jave bases CLI application built to manage and track individuals affected by disasters. The database uses PostgreSQL relational database and include JUnit testing. Built for ENSF 380 Final Project. Grade: 95%"}
             url = {"https://github.com/LReed03/ENSF380FinalProject"}
             title = {"Disaster Victim Relief System"}/>
         </div>
