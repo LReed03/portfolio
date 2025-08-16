e41c2a74fb805a7e3f2038d03d9577db2d4fbdfb
Landon Reed <144170201+LReed03@users.noreply.github.com>
2025-08-15 21:30:22 -0600

    Update About.js

--- src/About.js
+++ src/About.js
@@ -13,7 +13,7 @@ function About(){
                 </Reveal>
                 <Reveal direction="right">
                     <div className="text">
-                        <h5>Hi, I'm' Landon Reed!</h5>
+                        <h5>Hi, I'm Landon Reed!</h5>
                         <p>I’m a third-year Software Engineering student at the University of Calgary passionate about building practical, user-focused applications. I specialize in full-stack development with React, Flask, and Java, with growing expertise in PostgreSQL and AI-assisted tools. I love transforming ideas into clean, efficient, and intuitive software — from the first line of code to the final deployment.
                             <br/><br/>When I’m not coding, I’m often training in the gym or exploring creative outlets like Lego and music, which inspire my problem-solving and attention to detail.
                             <br/><br/>Explore my projects or view my resume to learn more..</p>
